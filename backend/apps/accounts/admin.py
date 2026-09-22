1da57a9315a7f4684da0e707ddef899e5a5109ac
Donkor James <jamesdonkor987@gmail.com>
2026-09-22 18:31:06 +0000

    updated userAdmin

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -22,7 +22,7 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
         }),
     )
-    ordering = ['-date_joined']
+    ordering = ['-created_at']
 
 
 @admin.register(Address)
